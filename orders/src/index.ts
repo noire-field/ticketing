a96db6a4698ec6262ebad504790692f664ff7dbe
Noire Bladefield <noirefield@gmail.com>
2021-05-06 15:27:41 +0700

    Added Startup Message Orders

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,6 +7,8 @@ import { ExpirationCompletedListener } from './events/listeners/ExpirationComple
 import { PaymentCreatedListener } from './events/listeners/PaymentCreatedListener';
 
 const Start = async () => {
+    console.log("[Orders] Starting...");
+    
     if(!process.env.JWT_KEY)
         throw new Error('Unable to load JWT_KEY from env.');
     if(!process.env.MONGO_URI)
